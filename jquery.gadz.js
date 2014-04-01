18ffd000e4031291452205623f97f6a9470ed25c
Richard Wills <richardwills90@gmail.com>
2014-04-01 14:47:55 +0100

    Now triggers on resize.

--- jquery.gadz.js
+++ jquery.gadz.js
@@ -6,18 +6,35 @@
 		}
 		var options =  $.extend(defaults, options);
 		
+		/*return this.each(function() {
+			var gadzList = $(this);
+			$(window).load(function(){resizeTiles(gadzList);});
+			if (options.maxWidth > 0){
+				$(window).resize(function () { 
+					 resizeTiles(gadzList);
+				});
+			} 
+		});*/
+		
 		
 		var gadzList =  $(this.selector);
 		$(window).load(function(){resizeTiles(gadzList);});
+				$(window).resize(function () {
+					 resizeTiles(gadzList);
+				});
 	
 		
 		function resizeTiles(gadzList){
+		
+			gadzList.each(function(){
+				$(this).height('');
+			});
 			var maxHeight = 0;
 			gadzList.each(function(){
 				maxHeight = Math.max(maxHeight, $(this).height());
 			});			
-			
-			
+		
+		
 			gadzList.each(function(){
 				$(this).height(maxHeight + 'px');
 			});
