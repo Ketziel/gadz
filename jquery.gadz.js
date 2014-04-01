48bf0f268fe339c73826970595f3a4f8c800e04a
Richard Wills <richardwills90@gmail.com>
2014-04-01 15:39:44 +0100

    Added functionality to trigger at certain browser widths.

--- jquery.gadz.js
+++ jquery.gadz.js
@@ -2,14 +2,25 @@
 	$.fn.gadz = function(options) {
 
 		var defaults = {
-			
+			startWidth : 0,
+			maxWidth : 0
 		}
 		var options =  $.extend(defaults, options);
 		
 		var gadzList =  $(this.selector);
-		$(window).load(function(){resizeTiles(gadzList);});
+		$(window).load(function(){
+			if(Math.max(document.documentElement.clientWidth, window.innerWidth || 0) > options.startWidth && (Math.max(document.documentElement.clientWidth, window.innerWidth || 0) < options.maxWidth || options.maxWidth == 0)){
+				resizeTiles(gadzList);
+			}
+		});
+		
 		$(window).resize(function () {
-			 resizeTiles(gadzList);
+			var x = Math.max(document.documentElement.clientWidth, window.innerWidth || 0);
+			if(x > options.startWidth && (x < options.maxWidth || options.maxWidth == 0)){
+				resizeTiles(gadzList);
+			} else {
+				resetTileSize(gadzList);
+			}
 		});
 		
 		function resizeTiles(gadzList){
@@ -18,10 +29,15 @@
 				$(this).height('');
 				maxHeight = Math.max(maxHeight, $(this).height());
 			});			
-		
 			gadzList.each(function(){
 				$(this).height(maxHeight + 'px');
 			});
+		}
+		
+		function resetTileSize(gadzList){		
+			gadzList.each(function(){
+				$(this).height('');
+			});		
 		}
 				
 	};
