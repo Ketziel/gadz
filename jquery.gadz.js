f41cfce2d63b2e11b712ec8b52b019cae65c7672
Richard Wills <richardwills90@gmail.com>
2014-04-01 14:51:58 +0100

    Removed redundant Loop.

--- jquery.gadz.js
+++ jquery.gadz.js
@@ -6,43 +6,23 @@
 		}
 		var options =  $.extend(defaults, options);
 		
-		/*return this.each(function() {
-			var gadzList = $(this);
-			$(window).load(function(){resizeTiles(gadzList);});
-			if (options.maxWidth > 0){
-				$(window).resize(function () { 
-					 resizeTiles(gadzList);
-				});
-			} 
-		});*/
-		
-		
 		var gadzList =  $(this.selector);
 		$(window).load(function(){resizeTiles(gadzList);});
-				$(window).resize(function () {
-					 resizeTiles(gadzList);
-				});
-	
+		$(window).resize(function () {
+			 resizeTiles(gadzList);
+		});
 		
 		function resizeTiles(gadzList){
-		
-			gadzList.each(function(){
-				$(this).height('');
-			});
 			var maxHeight = 0;
 			gadzList.each(function(){
+				$(this).height('');
 				maxHeight = Math.max(maxHeight, $(this).height());
 			});			
-		
 		
 			gadzList.each(function(){
 				$(this).height(maxHeight + 'px');
 			});
 		}
-		
-		
-
-
-					
+				
 	};
 })( jQuery );
